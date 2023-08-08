e976e86745cbdd2f214a971bc9168ee99f2a93b9
Luc van Donkersgoed <lucvandonkersgoed@gmail.com>
2023-08-09 00:17:54 +0200

    switch to live url

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 export async function getSessions() {
-  // const API_URL = "https://api.reinvent23.l15d.com/sessions";
-  const API_URL = "https://mzr7s3r3e8.execute-api.eu-west-1.amazonaws.com/prod/sessions";
+  const API_URL = "https://api.reinvent23.l15d.com/sessions";
+  // const API_URL = "https://mzr7s3r3e8.execute-api.eu-west-1.amazonaws.com/prod/sessions";
   const headers = { "Content-Type": "application/json" };
 
   const res = await fetch(API_URL, {
